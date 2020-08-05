3e3b76e5c17199cb037842c5957fcbf309c53a6a
David Elì <davidelimg1@gmail.com>
2020-08-05 17:35:03 -0500

    Added component interaction

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,20 +5,23 @@ import './Button.css';
 export default class Button extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
-    orange: PropTypes.bool,
+    color: PropTypes.string,
     wide: PropTypes.bool,
+    clickHandler: PropTypes.func.isRequired,
   };
+
+  handleClick = (buttonName) => this.props.clickHandler(buttonName);
 
   render() {
     const classNames = [
       'component-button',
-      this.props.orange ? 'orange' : '',
+      this.props.color === 'orange' ? 'orange' : '',
       this.props.wide ? 'wide' : '',
     ];
 
     return (
       <div className={classNames.join(' ').trim()}>
-        <button>{this.props.name}</button>
+        <button onClick={this.handleClick}>{this.props.name}</button>
       </div>
     );
   }
